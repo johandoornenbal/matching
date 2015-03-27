7295c67935cb8104b102df89e2c615c60b9d9c2a
Johan Doornenbal <johan@filternet.nl>
2015-03-27 12:18:39 +0100

    start communicationChannels

---
+++ dom/src/main/java/info/matchingservice/dom/CommunicationChannels/CommunicationChannel.java
@@ -0,0 +1,13 @@
+package info.matchingservice.dom.CommunicationChannels;
+
+import info.matchingservice.dom.MatchingDomainObject;
+
+public abstract class CommunicationChannel extends
+		MatchingDomainObject<CommunicationChannel> {
+
+	public CommunicationChannel(String keyProperties) {
+		super(keyProperties);
+		// TODO Auto-generated constructor stub
+	}
+
+}
