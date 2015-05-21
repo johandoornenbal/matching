a3249cbba86ff01a38867331942b228ce9d7ae2a
Johan Doornenbal <johan@filternet.nl>
2015-05-21 10:48:59 +0200

    small cosmetic commit

--- dom/src/main/java/info/matchingservice/dom/CommunicationChannels/CommunicationChannelContributions.java
+++ dom/src/main/java/info/matchingservice/dom/CommunicationChannels/CommunicationChannelContributions.java
@@ -36,8 +36,8 @@ public class CommunicationChannelContributions extends MatchingDomainService<Com
     @Action(semantics = SemanticsOf.NON_IDEMPOTENT)
     @ActionLayout(contributed = Contributed.AS_ACTION)
     public Person createPhone(
-            final @ParameterLayout(named = "Type") CommunicationChannelType type,
-            final @ParameterLayout(named = "Number")
+            final @ParameterLayout(named = "type") CommunicationChannelType type,
+            final @ParameterLayout(named = "phoneNumber")
 
             @Parameter(regexPattern ="(^(((0)[1-9]{2}[0-9][-| ]?[1-9]( ?[0-9]){5})|((\\+31|0|0031)[-| ]?[1-9][0-9][1-9]( ?[0-9]){6}))$)"
                     + "|(^(((\\\\+31|0|0031)6){1}[-| ]?[1-9]{1}( ?[0-9]){7})$)") String phoneNumber,
